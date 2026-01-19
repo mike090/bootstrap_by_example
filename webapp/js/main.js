3645e2b34a391a449a7ecf416a57196771c520a1
mike09 <mike09@mail.ru>
2026-01-19 17:31:39 +0300

    Add modal

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+  var $charCount, maxCharCount;
 
+  $charCount = $('#tweet-modal .char-count')
+  maxCharCount = parseInt($charCount.data('max'), 10);
+
+  $('#tweet-modal textarea').on('keyup', function(e) {
+    var tweetLength = $(e.currentTarget).val().length;
+
+    $charCount.html(maxCharCount - tweetLength);
+  });
 });
