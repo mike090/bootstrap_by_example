54eb44600846e8b7ccee193eeb59d5c264673ecb
mike09 <mike09@mail.ru>
2026-01-19 18:30:33 +0300

    Add popover panel

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -11,4 +11,29 @@ $(document).ready(function() {
   });
 
   $('[data-toggle="tooltip"]').tooltip();
+
+  var popoverContentTemplate = '' +
+  '<img src="imgs/breed.jpg" class="img-rounded">' +
+  '<div class="info">' +
+    '<strong>Dog Breeds</strong>' +
+    '<a href="#" class="btn btn-default">' +
+      '<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>' +
+      'Follow' +
+    '</a>' +
+  '</div>'
+
+  $('[data-toggle="popover"]').popover({
+    placement: 'bottom',
+    html: true,
+    content: function() {
+      return popoverContentTemplate;
+    }
+  });
+
+  $('[data-toggle="popover"]').on('show.bs.popover', function() {
+    var $icon = $(this).find('span.glyphicon');
+
+    $icon.removeClass('glyphicon-plus').addClass('glyphicon-ok');
+    $(this).append('ing');
+  });
 });
